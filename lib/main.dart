d3fa956a45708483e79b6940255f9df9b513accd
vik2573 <va-demen@yandeex.ru>
2021-06-10 20:21:58 +0400

    scaffold commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,18 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text(
-          "Hello Flutte",
-          style: TextStyle(fontSize: 30),
-          textDirection: TextDirection.ltr,
-        ),
+        home: Scaffold(
+      backgroundColor: Colors.indigo,
+      appBar: AppBar(
+        title: Text("My First App"),
+        centerTitle: true,
       ),
-    ),
+      body: Center(
+          child: Text(
+        "This is My Homepage",
+        style: TextStyle(
+            fontSize: 30, fontWeight: FontWeight.bold, color: Colors.white),
+      )),
+    )),
   );
 }
