111ee2d27e25f2f3f12e3463092a016fa6f0a54c
vik2573 <va-demen@yandeex.ru>
2021-06-11 17:11:55 +0400

    Stateless Widget commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,49 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-        home: Scaffold(
-      backgroundColor: Colors.indigo,
-      appBar: AppBar(
-        title: Text("My First App"),
-        centerTitle: true,
+void main() => runApp(MyFirstApp());
+
+class MyFirstApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.indigo,
+        appBar: AppBar(
+          title: Text("My first App"),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Container(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                LinearProgressIndicator(
+                  value: 23,
+                ),
+                Text(
+                  "23 %",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                ),
+                Text(
+                  "Press button to download",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: null,
+          child: Icon(Icons.cloud_download),
+        ),
       ),
-      body: Center(
-          child: Text(
-        "This is My Homepage",
-        style: TextStyle(
-            fontSize: 30, fontWeight: FontWeight.bold, color: Colors.white),
-      )),
-    )),
-  );
+    );
+  }
 }
