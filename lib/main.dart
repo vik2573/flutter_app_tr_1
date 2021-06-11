a3f68d6c5c1d7e50dd10ada150e3f4dc9a7d9f84
vik2573 <va-demen@yandeex.ru>
2021-06-11 18:53:03 +0400

    Stateful Widget Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,27 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyFirstApp());
 
-class MyFirstApp extends StatelessWidget {
+class MyFirstApp extends StatefulWidget {
+  @override
+  _MyFirstAppState createState() {
+    return _MyFirstAppState();
+  }
+}
+
+class _MyFirstAppState extends State<MyFirstApp> {
+  late bool _loading;
+  late double _progressValue;
+
+  @override
+  void initState() {
+    _loading = false;
+    _progressValue = 0.0;
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,35 +34,57 @@ class MyFirstApp extends StatelessWidget {
         body: Center(
           child: Container(
             padding: EdgeInsets.all(16),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                LinearProgressIndicator(
-                  value: 23,
-                ),
-                Text(
-                  "23 %",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
+            child: _loading
+                ? Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      LinearProgressIndicator(
+                        value: _progressValue,
+                      ),
+                      Text(
+                        "${(_progressValue * 100).round()} %",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  )
+                : Text(
+                    "Press button to download",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),
                   ),
-                ),
-                Text(
-                  "Press button to download",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                  ),
-                ),
-              ],
-            ),
           ),
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: null,
+          onPressed: () {
+            setState(() {
+              _loading = !_loading;
+              _updateProgress();
+            });
+          },
           child: Icon(Icons.cloud_download),
         ),
       ),
     );
+  }
+
+  void _updateProgress() {
+    const oneSec = const Duration(seconds: 1);
+    Timer.periodic(oneSec, (Timer t) {
+      setState(() {
+        _progressValue += 0.2;
+
+        if (_progressValue.toStringAsFixed(1) == "1.0") {
+          _loading = false;
+          t.cancel();
+          _progressValue = 0.0;
+          return;
+        }
+      });
+    });
   }
 }
